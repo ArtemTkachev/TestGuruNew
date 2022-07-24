fa71540f6f09d8ac201bd9a8d3ed0fed37ddeb60
ArtemTkachev <art59926@gmail.com>
2022-07-24 20:48:20 +0300

    fix: add dependent to users_tests

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,7 +2,7 @@ class Test < ApplicationRecord
   belongs_to :category
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   has_many :questions, dependent: :destroy
-  has_many :users_tests
+  has_many :users_tests, dependent: :destroy
   has_many :users, through: :users_tests
 
   scope :by_category_title,
